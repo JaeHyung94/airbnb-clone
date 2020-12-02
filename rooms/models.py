e806c47d7408d321d817d84b215ebf7e043fb3a7
JaeHyung_iMac <jhpark.main@gmail.com>
2020-12-02 21:33:02 +0900

    Reservation admin 1

--- rooms/models.py
+++ rooms/models.py
@@ -57,7 +57,7 @@ class Photo(core_models.TimeStampedModel):
 
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
@@ -67,6 +67,7 @@ class Room(core_models.TimeStampedModel):
 
     """Room Model Definition"""
 
+    # Related_name == For Targets!!
     name = models.CharField(max_length=140)
     description = models.TextField()
     country = CountryField()
@@ -83,10 +84,22 @@ class Room(core_models.TimeStampedModel):
     host = models.ForeignKey(
         user_models.User, related_name="rooms", on_delete=models.CASCADE
     )
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity, blank=True)
-    facilities = models.ManyToManyField(Facility, blank=True)
-    house_rules = models.ManyToManyField(HouseRule, blank=True)
+    room_type = models.ForeignKey(
+        RoomType, related_name="romms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField(Amenity, related_name="rooms", blank=True)
+    facilities = models.ManyToManyField(Facility, related_name="romms", blank=True)
+    house_rules = models.ManyToManyField(HouseRule, related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
+
+    def total_rating(self):
+        all_reviews = self.reviews.all()
+        all_ratings = 0
+        try:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+            return all_ratings / len(all_reviews)
+        except ZeroDivisionError:
+            return 0
