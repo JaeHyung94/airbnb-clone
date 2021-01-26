d943be7007d5f7a462756d370e2498bc423a75b5
JaeHyung_iMac <jhpark.main@gmail.com>
2021-01-26 16:54:05 +0900

    stataic files 3

--- config/settings.py
+++ config/settings.py
@@ -158,9 +158,9 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 AUTH_USER_MODEL = "users.User"
 
