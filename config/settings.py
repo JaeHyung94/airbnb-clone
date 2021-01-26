0b26a37e4b5e2b630689b5a335722129363d8b16
JaeHyung_iMac <jhpark.main@gmail.com>
2021-01-26 16:26:38 +0900

    static files

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,11 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+THIRD_PARTY_APPS = [
+    "django_countries",
+    "django_seed",
+    "storages",
+]
 
 
 PROJECT_APPS = [
@@ -170,6 +174,7 @@ EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
 EMAIL_FROM = "air-bnb-admin@sandboxba5253d512e34430897a89ad88167775.mailgun.org"
 
+
 # Auth
 
 LOGIN_URL = "/users/login"
@@ -182,6 +187,17 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 # Sentry
 
 if not DEBUG:
+
+    DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_KEY")
+    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-2-jh"
+    AWS_DEFAULT_ACL = "public-read"
+
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
+
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
         integrations=[DjangoIntegration()],
